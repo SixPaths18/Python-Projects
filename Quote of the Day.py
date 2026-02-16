d71338c6bda6a021a66b25808fbf818c6ae74aca
SixPaths18 <neelpatel31709@yahoo.com>
2026-02-16 19:02:09 +0200

    Add files via upload

---
+++ Quote of the Day.py
@@ -0,0 +1,12 @@
+import random
+
+# Storing quotes
+quotes = [
+    "Keep going, you're doing great 💪",
+    "Small steps every day!",
+    "Discipline beats motivation.",
+    "God is with you.",
+    "You are stronger than you think."
+]
+
+print(random.choice(quotes)) # Outputting a Quote of the Day
